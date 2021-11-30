42e0fc2514de4111a9c3dea1c3a4e7406159cf85
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-11-30 15:08:52 -0800

    save

---
+++ from_pytorch.cuh
@@ -0,0 +1,121 @@
+#pragma once
+#include "AsmUtils.cuh"
+#include "DeviceUtils.cuh"
+
+template <typename scalar_t>
+struct TopKTypeConfig {};
+
+template <>
+struct TopKTypeConfig<float> {
+  typedef uint32_t RadixType;
+
+  // Converts a float to an integer representation with the same
+  // sorting; i.e., for floats f1, f2:
+  // if f1 < f2 then convert(f1) < convert(f2)
+  // We use this to enable radix selection of floating-point values.
+  // This also gives a relative order for NaNs, but that's ok, as they
+  // will all be adjacent
+  // neg inf: signbit=1 exp=ff fraction=0 --> radix = 0 00 ff..
+  // pos inf: signbit=0 exp=ff fraction=0 --> radix = 1 ff 00..
+  // pos nan: signbit=0 exp=ff fraction>0 --> radix = 1 ff x>0
+  // neg nan: signbit=1 exp=ff fraction>0 --> radix = 0 00 x<ff...
+  static inline __device__ RadixType convert(float v) {
+    RadixType x = __float_as_int(v);
+    RadixType mask = (x & 0x80000000) ? 0xffffffff : 0x80000000;
+
+    return (v == v) ? (x ^ mask) : 0xffffffff;
+  }
+
+  static inline __device__ float deconvert(RadixType v) {
+    RadixType mask = (v & 0x80000000) ? 0x80000000 : 0xffffffff;
+
+    return __int_as_float(v ^ mask);
+  }
+};
+
+template <>
+struct TopKTypeConfig<uint8_t> {
+  typedef uint32_t RadixType;
+
+  static inline __device__ RadixType convert(uint8_t v) {
+    return v;
+  }
+
+  static inline __device__ uint8_t deconvert(RadixType v) {
+    return v;
+  }
+};
+
+template <>
+struct TopKTypeConfig<int8_t> {
+  typedef uint32_t RadixType;
+
+  static inline __device__ RadixType convert(int8_t v) {
+    return 128u + v;
+  }
+
+  static inline __device__ int8_t deconvert(RadixType v) {
+    return v - 128;
+  }
+};
+
+template <>
+struct TopKTypeConfig<int16_t> {
+  typedef uint32_t RadixType;
+
+  static inline __device__ RadixType convert(int16_t v) {
+    static_assert(sizeof(short) == 2, "");
+    return 32768u + v;
+  }
+
+  static inline __device__ int16_t deconvert(RadixType v) {
+    return v - 32768;
+  }
+};
+
+template <>
+struct TopKTypeConfig<int32_t> {
+  typedef uint32_t RadixType;
+
+  static inline __device__ RadixType convert(int32_t v) {
+    static_assert(sizeof(int) == 4, "");
+    return 2147483648u + v;
+  }
+
+  static inline __device__ int32_t deconvert(RadixType v) {
+    return v - 2147483648u;
+  }
+};
+
+template <>
+struct TopKTypeConfig<int64_t> {
+  typedef uint64_t RadixType;
+
+  static inline __device__ RadixType convert(int64_t v) {
+    static_assert(sizeof(int64_t) == 8, "");
+    return 9223372036854775808ull + v;
+  }
+
+  static inline __device__ int64_t deconvert(RadixType v) {
+    return v - 9223372036854775808ull;
+  }
+};
+
+template <>
+struct TopKTypeConfig<double> {
+  typedef uint64_t RadixType;
+
+  static inline __device__ RadixType convert(double v) {
+    RadixType x = __double_as_longlong(v);
+    RadixType mask = -((x >> 63)) | 0x8000000000000000;
+    return (v == v) ? (x ^ mask) : 0xffffffffffffffff;
+  }
+
+  static inline __device__ double deconvert(RadixType v) {
+    RadixType mask = ((v >> 63) - 1) | 0x8000000000000000;
+    return __longlong_as_double(v ^ mask);
+  }
+};
+
+// Over what radix we are selecting values
+constexpr int RADIX_BITS = 2; // digits are base-(2 ^ RADIX_BITS)
--- main.cu
+++ main.cu
@@ -1,5 +1,65 @@
 #include <iostream>
+#include "from_pytorch.cuh"
+
+template<typename bitwise_t>
+__device__ __forceinline__ bitwise_t highBitsFrom(int start) {
+  bitwise_t ones = ~(bitwise_t(0));
+  bitwise_t lowBits = (bitwise_t(1) << bitwise_t(start)) - 1;
+  return ones & ~lowBits;
+}
+
+// This kernel must be launched with 1024 threads
+template<typename scalar_t, typename bitwise_t, typename index_t, int radix_bits, bool order, typename start_offset_t>
+__launch_bounds__(1024)
+__global__ void countRadixMultipleBlock(
+  scalar_t* data,
+  start_offset_t startOffset,
+  index_t sliceSize,
+  index_t withinSliceStride,
+  index_t numSlices,
+  int itemsPerBlock,
+  int startBit,
+  bitwise_t *desired,
+  index_t *counts  // counts for each slice for each radix, needs to be initialized 0 before this kernel
+) {
+  constexpr int radix_size = (1 << radix_bits);
+  constexpr int radix_mask = radix_size - 1;
+
+  int blocksPerSlice = (sliceSize + itemsPerBlock - 1) / itemsPerBlock;
+  int slice = blockIdx.x / blocksPerSlice;
+  if (slice >= numSlices) return;
+  int blockInSlice = blockIdx.x % blocksPerSlice;
+
+  scalar_t *sliceData = data + startOffset(slice);
+
+  // Each thread count number of items locally.
+  // There are 32 * 32 thread-local counts in each block.
+  bitwise_t desiredMask = highBitsFrom<bitwise_t>(startBit + radix_bits);
+  index_t local_counts[radix_size] = {0};
+  for (index_t i = blockInSlice * 1024 + threadIdx.x; i < sliceSize; i += blocksPerSlice * 1024) {
+    scalar_t raw_value = doLdg(sliceData + withinSliceStride * i);
+    bitwise_t value = TopKTypeConfig<scalar_t>::convert(raw_value);
+    bool hasVal = ((value & desiredMask) == desired[slice]);
+    if (hasVal) {
+      bitwise_t digitInRadix = at::cuda::Bitfield<bitwise_t>::getBitfield(value, startBit, radix_bits);
+      #pragma unroll
+      for (uint32_t j = 0; j < radix_size; ++j) {
+        local_counts[j] +=  static_cast<int>(digitInRadix == j);
+      }
+    }
+  }
+
+  // Each warp merge its 32 thread local counts into one,
+  // Reducing 32 * 32 counts into 32 counts.
+  int warp = threadIdx.x / 32;
+  int lane = threadIdx.x % 32;
+
+  // The first warp merge all remaining 32 counts into one.
+
+  // The first thread uses atomic add to update global counts.
+}
+
 
 int main() {
-    std::cout << "Hello World!!!" << std::endl;
+  std::cout << "Hello World!!!" << std::endl;
 }
